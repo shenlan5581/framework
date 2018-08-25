e8b3a5fe4d4339a2a8baa117d8df3837d25a342b
ki <ki@ki.ki>
2018-08-25 09:50:53 +0800

    ki;

--- templates_c/12c8aa00d1632b70a226cf91988c5151ec274216_0.file.head.html.php
+++ templates_c/12c8aa00d1632b70a226cf91988c5151ec274216_0.file.head.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.32, created on 2018-08-24 11:04:57
+/* Smarty version 3.1.32, created on 2018-08-25 01:36:25
   from '/home/ki/https/www/framework/mvc/view/Index/layout/head.html' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.32',
-  'unifunc' => 'content_5b7fe659c0db24_49702057',
+  'unifunc' => 'content_5b80b299204be4_64811144',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '12c8aa00d1632b70a226cf91988c5151ec274216' => 
     array (
       0 => '/home/ki/https/www/framework/mvc/view/Index/layout/head.html',
-      1 => 1535108696,
+      1 => 1535160983,
       2 => 'file',
     ),
   ),
@@ -20,108 +20,83 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5b7fe659c0db24_49702057 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5b80b299204be4_64811144 (Smarty_Internal_Template $_smarty_tpl) {
 ?><!DOCTYPE HTML>
 <html lang="en">
 <head>
-<?php echo '<script'; ?>
- src="/public/jquery.js"><?php echo '</script'; ?>
->
+<link  href="/storage/a.css" rel="stylesheet">
+<link  href="/public/bootstrap/css/bootstrap.css" rel="stylesheet">
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <!-- bootstrap-->
- 
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
 
-<style>
+<style> 
 body{
-  margin-left:5px;
-  margin-right:5px;
-
-}
-
-
-#menu{
-  text-align: center;
- 
-}
-logo{
-  font-size: 20px;
-  display: block;
-  text-align: center;
-  color:rgb(4, 4, 4);
   margin:auto;
+  background-color: rgb(237, 237, 237);
+  width:80%
 }
+
+k-logo{
+  font-size: 20px;
+  display: inline-block;
+  color:rgb(4, 4, 4);
+}
+header{
+   display:block;
+   width:100%;
+   background-color: rgb(255, 255, 255);
+   height: 100px;
+   box-shadow: 5px 5px 12px #888888;
+}
+#k-menu{
+   position:relative;
+   margin-left:30%;
+   display: inline-block;
+   font-size: 15px;
+   top:40px;;
+}
+#k-user{
+   position:relative;
+   margin-left:90%;
+   display: inline-block;
+   font-size: 13px;
+}
+
 
 </style>
  
-
-
 <title>home</title>
-</head> 
+</head>
 <body>
  <header> <!--头部-->
- <logo id ='logo'> <!--logo-->
-  <img src="/sites/Index/logo.jpg" style="padding:5px;width:50px;;height:auto"></img>
-  <span>三原色装饰工程有限公司 </span> <span style="font-size: 13px;color:rgb(211, 164, 44);" > 打造完美家居</span>  
- </logo>
- <div id = 'menu'>
-
-    <nav class="navbar navbar-expand-lg navbar-light" style="background-color:rgb(235, 235, 235)">
-        <a class="navbar-brand" href="#"></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">工程案例<span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">设计</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">预约</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">公司简介</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-
+  <logo id ='k-logo'> <!--logo-->
+      <img src="/sites/Index/logo.jpg" style="padding:22px;width:50px;height:auto"></img>
+  </logo>
+  <span  style="position:absolute;top:40px;;color:rgb(67, 65, 67)">三原色 装饰工程有限公司 </span> 
+  <span style="font-size:12px;color:rgb(8, 64, 160);position:absolute;top:59px;" > 为您打造完美家居</span>  
+<div id='k-menu' >    
+     <a class = 'a1' style="color:rgb(189, 57, 57)" href="#1">工程案例</a>
+     <a class = 'a1' href="#1">设计效果</a>
+     <a class = 'a1' href="#1">风格简介</a>
+     <a class = 'a1' href="#1">样板间</a>
+     <a class = 'a1' href="#1">预约</a>
+     <a class = 'a1' href="#1">公司简介</a>
  </div><!--/menu end -->
-
+ <div id='k-user'>
+     <a class = 'a2' href="#1">登陆</a>
+     <a class = 'a2' href="#1">注册</a>
  </div>
-<br>
- <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-    <ol class="carousel-indicators">
-      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-    </ol>
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img class="d-block w-100" src="/sites/Manage/1.jpg" alt="First slide" style="height:500px;width:auto">
-      </div>
-      <div class="carousel-item">
-        <img class="d-block w-100" src="/sites/Manage/2.jpg" alt="Second slide"style="height:500px;width:auto">
-      </div>
-      <div class="carousel-item">
-        <img class="d-block w-100" src="/sites/Manage/3.jpg" alt="Third slide"style="height:500px;width:auto">
-      </div>
-    </div>
-    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
+   <div id='picture'>
+       <div>  </div>
+       <div>  </div>
+       <div>  </div>
+       <div>  </div>
   </div>
+ </div>
+
 </header>
 <?php }
 }
--- templates_c/a3bb724e73a7a7843790a2db011e285ed74094e8_0.file.index.html.php
+++ templates_c/a3bb724e73a7a7843790a2db011e285ed74094e8_0.file.index.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.32, created on 2018-08-24 11:08:34
+/* Smarty version 3.1.32, created on 2018-08-25 01:47:28
   from '/home/ki/https/www/framework/mvc/view/Index/index.html' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.32',
-  'unifunc' => 'content_5b7fe732328197_27888400',
+  'unifunc' => 'content_5b80b530f39c45_12458345',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'a3bb724e73a7a7843790a2db011e285ed74094e8' => 
     array (
       0 => '/home/ki/https/www/framework/mvc/view/Index/index.html',
-      1 => 1535108912,
+      1 => 1535161647,
       2 => 'file',
     ),
   ),
@@ -20,103 +20,131 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5b7fe732328197_27888400 (Smarty_Internal_Template $_smarty_tpl) {
-?>
-<style>
-.k-item{   
-    float: left;
-    width:350px;
-    margin:5px;
-    font-size:14px;
-}
-#k-template{
+function content_5b80b530f39c45_12458345 (Smarty_Internal_Template $_smarty_tpl) {
+?><style>
+		.k-item{   
+			float: left;
+			width:350px;
+			margin:5px;
+			font-size:14px;
 
-}
-.k-title{
-    font-size:20px;
-    padding:3px;
-    margin: 30px;
-    text-align: center;
-}
+			z-index:2;
+		}
+		#k-template{
+		}
+		.k-title{
+			color:rgb(63, 63, 63);
+			font-size:20px;
+			padding:3px;
+			margin: 30px;
+			text-align: center;
+			font-weight: 900;
+		}
+        .info{
+			  width:350px;
+			  height:250px;
+			  background-color: rgba(0, 0, 0, 0.523); 
+			  position:absolute;
+			  z-index: 3;
+			  text-align: center;
+			  cursor: pointer;
+			  display:none;
+		}
+		.info>span{
+			 position: absolute;
+			 display:block;
+			 z-index:1;
+			 color:white;
+			 top:46%;left:46%;
+		}
 
-</style>
+		</style>
+		
 
 
-<div id = "k-jiazhaung">
-   <div class='k-title'>
-    家装案例
-   </div>    
-   <div class="k-item">
-       <image src = "/sites/Index/2.jpg" width=350px height=250px;></image>
-       <span>理想名称 现代风格</span>
-   </div>
-   <div class="k-item">
-       <image src = "/sites/Index/3.jpg" width=350px height=250px;></image>
-       <span>理想名称 现代风格</span>
-   </div>
-   <div class="k-item">
-       <image src = "/sites/Index/4.jpg" width=350px height=250px;></image>
-       <span>理想名称 现代风格</span>
-   </div>
-   <div class="k-item">
-       <image src = "/sites/Index/5.jpg" width=350px height=250px;></image>
-       <span>理想名称 现代风格</span>
-   </div>
-   <div class="k-item">
-       <image src = "/sites/Index/6.jpg" width=350px height=250px;></image>
-       <span>理想名称 现代风格</span>
-   </div>
-   <div class="k-item">
-       <image src = "/sites/Index/7.jpg" width=350px height=250px;></image>
-       <span>理想名称 现代风格</span>
-   </div>
-
-</div>
+		<div class="row">
+			<div class="col-xs-6 col-md-3">
+			  <a href="#" class="thumbnail">
+				<image  "/sites/Index/2.jpg" alt="..."></image>
+			  </a>
+			</div>
+			...
+		  </div>
 
 
 
 
-<div style="clear:both"></div>
 
-<div id = "k-gongzhuang">
-   <div class='k-title'>
-     工装案例 
-   </div>    
-   <span>简介</span>
-   <div class="k-item">
-   </div>
-
-</div>
-</div>
-
-<div style="clear:both"></div>
-
-
-<?php echo '<script'; ?>
+		<div id = "k-jiazhaung">
+		   <div class='k-title'>
+			家装案例
+		   </div>    
+		   <div class="k-item">
+			   <div class="info"  > <span >详情 </span></div>
+			   <image src = "/sites/Index/2.jpg" width=350px height=250px;></image>
+		   </div>
+		   <div class="k-item">
+			   <div class="info"  > <span  >查看详情</span></div>
+			   <image src = "/sites/Index/3.jpg" width=350px height=250px;></image>
+		   </div>
+		   <div class="k-item">
+			   <div class="info"  > <span  >简介</span></div>
+			   <image src = "/sites/Index/4.jpg" width=350px height=250px;></image>
+		   </div>
+		   <div class="k-item">
+			   <div class="info"  > <span  >简介</span></div>
+			   <image src = "/sites/Index/5.jpg" width=350px height=250px;></image>
+		   </div>
+		   <div class="k-item">
+			   <div class="info"  > <span  >简介</span></div>
+			   <image src = "/sites/Index/6.jpg" width=350px height=250px;></image>
+		   </div>
+		   <div class="k-item">
+			   <div class="info"  > <span  >简介</span></div>
+			   <image src = "/sites/Index/7.jpg" width=350px height=250px;></image>
+		   </div>
+		
+		</div>
+		
+		
+		
+		
+		<div style="clear:both"></div>
+		
+		<div id = "k-gongzhuang">
+		   <div class='k-title'>
+			 工装案例 
+		   </div>    
+		   <span>简介</span>
+		   <div class="k-item">
+		   </div>
+		
+		</div>
+		</div>
+		
+		<div style="clear:both"></div>
+		
+		
+		<?php echo '<script'; ?>
 >
-var obj=null;
-$('.k-item').mousemove(function(event){
-        event = event ? event : window.event;
-        var obj = event.srcElement ? event.srcElement : event.target; 
-        var $obj = $(obj);
-        var d =  $obj.children("div");
-        if(d == status_show){
-           return;
-        }
-        d.css("top",x);                
-        d.css("left",y);
-        d.fadeIn();                
-        if(status_show !== false){
-           status_show.fadeOut();
-           status_show=false;
-        }
-        status_show = d;
-     });
-    });
-});
+		$('.k-item').mousemove(function(event){
+				event = event ? event : window.event;
+				var obj = event.srcElement ? event.srcElement : event.target; 
+				var $obj = $(obj);
+				$obj = $obj.parent();
+				$obj = $obj.children('.info');
+				   $obj.fadeIn('fast');
+				});
+		$('.info').mouseleave(function(event){
+				event = event ? event : window.event;
+				var obj = event.srcElement ? event.srcElement : event.target; 
+				var $obj = $(obj);
+				$obj = $obj.parent();
+				$obj = $obj.children('.info');
+				    $obj.fadeOut('fast'); 
+		});
 
-
-
-<?php echo '</script'; ?>
-><?php }
+		<?php echo '</script'; ?>
+>
+<?php }
 }
