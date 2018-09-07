3a1213b626fb29d8d465c3d8b5def8d31b19d6f9
vaio <22542812@qq.com>
2018-09-08 05:55:14 +0800

    k

---
+++ mvc/controller/Wxccsj/Home.php
@@ -0,0 +1,13 @@
+<?php
+
+
+class App_Controller_Wxccsj_Home{
+  
+    public function wx_infoAction{
+      
+       echo "KK";
+
+
+
+    }
+}
---
+++ templates_c/bc9c322c7d27cbba0f882979ded6310a81f6c555_0.file.CaseMenu.html.php
@@ -0,0 +1,28 @@
+<?php
+/* Smarty version 3.1.32, created on 2018-09-07 21:26:53
+  from '/home/k/https/www/framework/mvc/view/Manage/Case/CaseMenu.html' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.32',
+  'unifunc' => 'content_5b92ed1d4cf2b7_33256139',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'bc9c322c7d27cbba0f882979ded6310a81f6c555' => 
+    array (
+      0 => '/home/k/https/www/framework/mvc/view/Manage/Case/CaseMenu.html',
+      1 => 1536355386,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5b92ed1d4cf2b7_33256139 (Smarty_Internal_Template $_smarty_tpl) {
+?><nav class="nav">
+    <a style="font-weight:900;   " class="nav-link active" href="/Manage/Case/CaseEdit">创建</a>
+    <a class="nav-link disabled" href="#">Disabled</a>
+  </nav><?php }
+}
