e1958216e18572dee3bc40612b61a0826e3f16ae
ki <ki@ki.ki>
2018-09-18 17:28:32 +0800

    ki

---
+++ project/frame/templates_c/8ac6089ad9c8820c5d8b47cf16c03f0df25c20da_0.file.foot.html.php
@@ -0,0 +1,58 @@
+<?php
+/* Smarty version 3.1.32, created on 2018-09-18 09:27:20
+  from '/home/ki/https/www/framework/project/frame/mvc/view/Index/layout/foot.html' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.32',
+  'unifunc' => 'content_5ba0c4f830fcc7_20191284',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '8ac6089ad9c8820c5d8b47cf16c03f0df25c20da' => 
+    array (
+      0 => '/home/ki/https/www/framework/project/frame/mvc/view/Index/layout/foot.html',
+      1 => 1537197353,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5ba0c4f830fcc7_20191284 (Smarty_Internal_Template $_smarty_tpl) {
+?><style>
+#foot{
+  display:none;
+  background-color: rgba(255, 255, 255, 0.749);
+  height: 100px;
+  text-align: center;
+  padding: 10px;
+  color:rgb(155, 154, 154);
+  font-size: 12px;
+}
+
+
+
+</style>
+
+
+
+
+
+<div id ="foot">
+   <span>璀璨</span><br>
+   <span>备案号</span>
+</div>
+
+
+ 
+<?php echo '<script'; ?>
+ src="/public/jquery-3.3.1.min.js"><?php echo '</script'; ?>
+>
+<?php echo '<script'; ?>
+ src="/public/bootstrap/js/bootstrap.min.js"><?php echo '</script'; ?>
+>
+</body>
+</html><?php }
+}
---
+++ project/frame/templates_c/e687a2124c087ac35728c9be8e7dda49210521b0_0.file.index.html.php
@@ -0,0 +1,47 @@
+<?php
+/* Smarty version 3.1.32, created on 2018-09-18 09:27:20
+  from '/home/ki/https/www/framework/project/frame/mvc/view/Index/index.html' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.32',
+  'unifunc' => 'content_5ba0c4f830ee87_79626296',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'e687a2124c087ac35728c9be8e7dda49210521b0' => 
+    array (
+      0 => '/home/ki/https/www/framework/project/frame/mvc/view/Index/index.html',
+      1 => 1537197315,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5ba0c4f830ee87_79626296 (Smarty_Internal_Template $_smarty_tpl) {
+?><style>
+	#sanyuanse{
+	text-align: center;
+	display: block;
+  width:150px;
+  height:auto;
+	transform:rotate(-5deg);
+	font-size:8;
+	color:rgb(149, 149, 149);
+	}
+	.logo{
+  width:150px;
+  height:auto;
+	}
+
+</style>
+
+
+<a id='sanyuanse'  href="/sanyuanse/Manage">
+	<img class="logo" src="/sanyuanse/logo.jpg"><img>
+	郑州三原色 
+</a>
+<?php }
+}
