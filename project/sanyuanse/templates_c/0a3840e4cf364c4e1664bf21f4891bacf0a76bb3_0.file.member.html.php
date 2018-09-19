04daea611182cc4c6faaa665909685c2c1ec4ccb
ki <ki@ki.ki>
2018-09-20 06:26:11 +0800

    ki

--- project/sanyuanse/templates_c/0a3840e4cf364c4e1664bf21f4891bacf0a76bb3_0.file.member.html.php
+++ project/sanyuanse/templates_c/0a3840e4cf364c4e1664bf21f4891bacf0a76bb3_0.file.member.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.32, created on 2018-09-19 21:45:04
+/* Smarty version 3.1.32, created on 2018-09-19 22:26:04
   from '/home/ki/https/www/framework/project/sanyuanse/mvc/view/Wechat/member.html' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.32',
-  'unifunc' => 'content_5ba2c36011e3f1_29584159',
+  'unifunc' => 'content_5ba2ccfca76282_89449082',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '0a3840e4cf364c4e1664bf21f4891bacf0a76bb3' => 
     array (
       0 => '/home/ki/https/www/framework/project/sanyuanse/mvc/view/Wechat/member.html',
-      1 => 1537393503,
+      1 => 1537395963,
       2 => 'file',
     ),
   ),
@@ -20,33 +20,31 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5ba2c36011e3f1_29584159 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5ba2ccfca76282_89449082 (Smarty_Internal_Template $_smarty_tpl) {
 ?> 
 <!DOCTYPE HTML>
 <html lang="en">
 <head>
-<?php echo '<script'; ?>
- src="/public/jquery-2.1.3.min.js"><?php echo '</script'; ?>
->
-<?php echo '<script'; ?>
- src="/Wechat/wx.js"><?php echo '</script'; ?>
->
 
+        <link rel="stylesheet" href="/public/weui/dist/style/weui.min.css"/>
+        <link rel="stylesheet" href="/storage/k.css"/>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
 
-<title>Wx</title>
+<title>会员</title>
 </head> 
 <body >
 
 <style>
     body{
+        display:flex;
         font-size:50px;
-     
-   
         margin:10px;
+        flex-direction:column;
+        justify-content:conter;
+        align-items:center;
     }
         .logo {
-      width:90%;
+      width:80%;
       height:auto;
     }
     .part{
@@ -55,12 +53,33 @@ function content_5ba2c36011e3f1_29584159 (Smarty_Internal_Template $_smarty_tpl)
         justify-content:conter;
         align-items:center;
         color:rgb(138, 138, 138);
+        margin-bottom: 100px;
     }
     .partrait{
       border-radius: 50%;
       width:300px;
       margin:50px;
+    } 
+    .integral{
+        display:flex;
+        flex-direction:row;
+        margin: 3%;
+        color:rgb(149, 104, 15);
     }
+    .integral2{
+        width:80%;
+        padding:5%;
+        display:flex;
+        flex-direction:row;
+        margin: 3%;
+        color:rgb(130, 129, 126);
+    }
+    .nv{
+
+        width:100%;
+    }
+
+
 
 
 
@@ -83,17 +102,21 @@ function content_5ba2c36011e3f1_29584159 (Smarty_Internal_Template $_smarty_tpl)
                 </div>
         </div>
 
-  
+       <div class = "integral">
+            <div> 当前积分; </div>
+            <div class ="text2"> 999</div>
+       </div>
+            <div> 积分福利 </div>
+       <div class = "integral2 border02">
+            <div> 
+             集满1000兑换
+            </div>
+       </div>
 
-    
-        <div class="weui-flex">
-                <div class="weui-flex__item"><div class="placeholder">weui</div></div>
-                <div class="weui-flex__item"><div class="placeholder">weui</div></div>
-                <div class="weui-flex__item"><div class="placeholder">weui</div></div>
-                <div class="weui-flex__item"><div class="placeholder">weui</div></div>
-        </div>
-
-
+ <div class ='nv' >
+      <a href="javascript:;" class="weui-btn k-btn2 weui-btn_primary">会员签到</a>
+      <a href="javascript:;" class="weui-btn k-btn2 weui-btn_primary">查看工地进度</a>
+</div>
 
 </body>
 </html>
