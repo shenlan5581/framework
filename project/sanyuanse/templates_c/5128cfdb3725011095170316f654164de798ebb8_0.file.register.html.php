f2cd2470ae9e76e409d7b057aad31a7c83d41812
ki <ki@ki.ki>
2018-09-20 04:08:11 +0800

    ki

--- project/sanyuanse/templates_c/5128cfdb3725011095170316f654164de798ebb8_0.file.register.html.php
+++ project/sanyuanse/templates_c/5128cfdb3725011095170316f654164de798ebb8_0.file.register.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.32, created on 2018-09-19 19:29:45
+/* Smarty version 3.1.32, created on 2018-09-19 20:07:52
   from '/home/ki/https/www/framework/project/sanyuanse/mvc/view/Wechat/register.html' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.32',
-  'unifunc' => 'content_5ba2a3a984b332_79542412',
+  'unifunc' => 'content_5ba2ac98a97102_28835815',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '5128cfdb3725011095170316f654164de798ebb8' => 
     array (
       0 => '/home/ki/https/www/framework/project/sanyuanse/mvc/view/Wechat/register.html',
-      1 => 1537385379,
+      1 => 1537387671,
       2 => 'file',
     ),
   ),
@@ -20,31 +20,32 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5ba2a3a984b332_79542412 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5ba2ac98a97102_28835815 (Smarty_Internal_Template $_smarty_tpl) {
 ?><link rel="stylesheet" href="/public/weui/dist/style/weui.min.css"/>
 <link rel="stylesheet" href="/storage/k.css"/>
 
 <style>
     .boss{
-        display: flex;
+        font-size:40px;
+        display:flex;
         flex-direction:column;
         justify-content:conter;
         align-items:center;
         margin:10px;
     }
     .input1 {
-        width: 280px;;
-        height:50px;
+        width: 600px;;
+        height:80px;
         border:1px solid #aaa;
-        border-radius: 3px;
-        margin:10px;
+        border-radius:10px;
+        margin-top:50px;
+        margin-bottom:100px;
     }
     .logo {
-      width:60%;
+      width:100%;
       height:auto;
     }
     .member{
-        height:260px;
         padding:10px;
         color:rgb(160, 8, 8);
     }
@@ -54,18 +55,20 @@ function content_5ba2a3a984b332_79542412 (Smarty_Internal_Template $_smarty_tpl)
 <div class ="boss">
   <img class = "logo" src = "/sanyuanse/logo.jpg"></img>
  <div class ='member'>
-     <span>会员权益 </span>
-     <div>
-          
-
-
+     <span>会员权益:</span>
+     <div class ='content1'>
+         <ul>
+           <li>查看工程进度 </li>   
+           <li>会员折扣 </li>   
+           <li>优惠大礼包 </li>   
+         </ul>
      </div>
  </div>
  <div class ="text1">输入手机号码即刻成为会员</div>
  <form>
     <input class="input1" class="weui-input" type="tel" placeholder="请输入手机号">
-    <a style ="width:50%" href="javascript:;" class="weui-btn weui-btn_primary">确定</a>
+    <a href="javascript:;" class="weui-btn k-btn weui-btn_primary">确定</a>
   </form>
 </div>
-<?php }
+ <?php }
 }
