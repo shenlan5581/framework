84c32f42a923825cee1960ec8a1f28c1867d23dc
ki <ki@ki.ki>
2018-09-20 19:14:31 +0800

    ki

--- project/sanyuanse/templates_c/30e3db5138ff2e1a7245a7ea289f38374c212c9f_0.file.foot.html.php
+++ project/sanyuanse/templates_c/30e3db5138ff2e1a7245a7ea289f38374c212c9f_0.file.foot.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.32, created on 2018-09-20 11:11:07
+/* Smarty version 3.1.32, created on 2018-09-20 11:14:24
   from '/home/ki/https/www/framework/project/sanyuanse/mvc/view/Wechat/foot.html' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.32',
-  'unifunc' => 'content_5ba3804bbd1f91_04096286',
+  'unifunc' => 'content_5ba3811094fee4_65187372',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '30e3db5138ff2e1a7245a7ea289f38374c212c9f' => 
     array (
       0 => '/home/ki/https/www/framework/project/sanyuanse/mvc/view/Wechat/foot.html',
-      1 => 1537441866,
+      1 => 1537442063,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5ba3804bbd1f91_04096286 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5ba3811094fee4_65187372 (Smarty_Internal_Template $_smarty_tpl) {
 ?>
 
 
@@ -30,7 +30,7 @@ function content_5ba3804bbd1f91_04096286 (Smarty_Internal_Template $_smarty_tpl)
 foot{
     width:60%;
     margin-top:80px;
-    font-size:0.7em;
+    font-size:0.8em;
     display:flex;
     flex-direction:column;
     justify-content:conter;
@@ -39,14 +39,14 @@ foot{
     border-top: 1px solid #aaa;
 }
 foot>p{
-    font-size:0.7em;
+    font-size:0.8em;
     color:rgb(108, 106, 106);
 }
     
 </style> 
 <foot>
     <span>郑州三原色</span>
-    <p>郑州市上街区徐长路中段</p>
+    <p>郑州市上街区许昌路中段</p>
   
 </foot>
  
