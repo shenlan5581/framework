7939d51321534ebc69cbeffcd7d9a1132ce7eced
ki <ki@ki.ki>
2018-09-21 21:16:22 +0800

    ki

--- project/sanyuanse/templates_c/c4be60f1d2bc09eab84e58ec8580746ce00adde0_0.file.head.html.php
+++ project/sanyuanse/templates_c/c4be60f1d2bc09eab84e58ec8580746ce00adde0_0.file.head.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.32, created on 2018-09-21 13:07:54
+/* Smarty version 3.1.32, created on 2018-09-21 13:16:07
   from '/home/ki/https/www/framework/project/sanyuanse/mvc/view/Wechat/head.html' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.32',
-  'unifunc' => 'content_5ba4ed2a23e073_77642811',
+  'unifunc' => 'content_5ba4ef177de476_70632871',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'c4be60f1d2bc09eab84e58ec8580746ce00adde0' => 
     array (
       0 => '/home/ki/https/www/framework/project/sanyuanse/mvc/view/Wechat/head.html',
-      1 => 1537535268,
+      1 => 1537535765,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5ba4ed2a23e073_77642811 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5ba4ef177de476_70632871 (Smarty_Internal_Template $_smarty_tpl) {
 ?> 
 <!DOCTYPE HTML>
 <html lang="en">
@@ -38,7 +38,7 @@ function content_5ba4ed2a23e073_77642811 (Smarty_Internal_Template $_smarty_tpl)
 <style>
     html{
        width:100vw;
-       font-size: 2vh;
+       font-size: 40px;;
        background:radial-gradient(rgb(255, 255, 255), rgb(255, 255, 255)80%);
    }
 
--- project/sanyuanse/templates_c/e8ffc2dc45cd560e346b423af0985a4bb712ab99_0.file.Reserve.html.php
+++ project/sanyuanse/templates_c/e8ffc2dc45cd560e346b423af0985a4bb712ab99_0.file.Reserve.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.32, created on 2018-09-21 12:49:53
+/* Smarty version 3.1.32, created on 2018-09-21 13:15:25
   from '/home/ki/https/www/framework/project/sanyuanse/mvc/view/Wechat/Reserve.html' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.32',
-  'unifunc' => 'content_5ba4e8f19f2424_26545014',
+  'unifunc' => 'content_5ba4eeed0f1f07_54744755',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'e8ffc2dc45cd560e346b423af0985a4bb712ab99' => 
     array (
       0 => '/home/ki/https/www/framework/project/sanyuanse/mvc/view/Wechat/Reserve.html',
-      1 => 1537534192,
+      1 => 1537535722,
       2 => 'file',
     ),
   ),
@@ -20,13 +20,13 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5ba4e8f19f2424_26545014 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5ba4eeed0f1f07_54744755 (Smarty_Internal_Template $_smarty_tpl) {
 ?><style>
 .ts-btn{
     color:rgb(216, 216, 216);
     border-style:none;
     background: repeating-linear-gradient(to bottom,rgb(54, 160, 230), rgb(28, 109, 208)80%,rgb(11, 54, 184));
-    font-size:2vh;
+    font-size:1.3em;
     width: 50vw;
     font-weight: 900;
     border-radius: 20px;
@@ -51,11 +51,11 @@ img{
     height:auto;
 }
 .input1 {
-        font-size:1.2em;
-        width: 60vw;
-        height:2em;
-        border:1px solid #aaa;
-        border-radius:10px;
+    font-size:1.2em;
+    width: 800px;;
+    height:140px;
+    border:1px solid #aaa;
+    border-radius:10px;
     }
 </style>
 <div class='logo-title'>
